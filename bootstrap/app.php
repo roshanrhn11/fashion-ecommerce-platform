9b97757c8b02a428e8f8f4edd051a74719882ded
roshanrhn11 <roshanrhn11@gmail.com>
2026-07-15 14:55:26 +0530

    Switch to SQLite for production portfolio and update app config

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,16 +3,6 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use Illuminate\Support\Facades\Artisan;
-
-// சர்வர் தொடங்கும் போதே பழைய அனைத்து லாராவெல் கேச் நினைவகங்களையும் கட்டாயமாக அழிக்கிறோம்
-if (app()->environment('production')) {
-    try {
-        Artisan::call('optimize:clear');
-    } catch (\Exception $e) {
-        // எரர் வந்தால் புறக்கணிக்கவும்
-    }
-}
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
