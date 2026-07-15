d7ca4db192fa02ae467fc60bb0a5586f3f184b02
roshanrhn11 <roshanrhn11@gmail.com>
2026-07-15 12:57:00 +0530

    Fix trust proxies middleware for Render deployment

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        // Render லோடு பேலன்ஸர் டிராஃபிக்கை அனுமதிக்கிறது
+        $middleware->trustProxies(at: '*'); 
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
